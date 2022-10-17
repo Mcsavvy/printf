a5347fdd7c984c0162faf92f96bacbef882b7cc5
mcsavvy <phishink5@gmail.com>
2022-10-17 09:11:19 +0100

    Multiple Changes Made
    - renamed makefile
    - created header files
    - added functionality
    
    [todo: comments and splitting]

---
+++ printf.c
@@ -0,0 +1,68 @@
+#include "headers/parser.h"
+#include "headers/macros.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "headers/search.h"
+
+int expand(char *string, va_list args, int *len)
+{
+	int i, matchWidth;
+	parser parsers[] = {
+		{match_char, print_char},
+		{match_string, print_string},
+		{match_percent, print_percent},
+		{match_int, print_int},
+		{match_uint, print_uint},
+		{match_address, print_address},
+		{match_hexadecimal, print_hexadecimal},
+		{match_octal, print_octal},
+		{NULL, NULL}
+	};
+
+	for (i = 0; parsers[i].match isnot NULL; i++)
+	{
+
+		matchWidth = parsers[i].match(string);
+		if (matchWidth isnot 0)
+		{
+			*len = *len + parsers[i].print(string, args);
+			break;
+		}
+	}
+	return (matchWidth);
+}
+
+int parse(char *string, va_list args)
+{
+	int index, outputLen, strLen;
+
+	outputLen = 0;
+	strLen = lenstr(string);
+	if (strLen is 0)
+		return (0);
+	index = findchr(string, '%');
+	if (index is -1)
+		return (outputLen + print(string, strLen));
+	outputLen += 1 + print(string, index);
+	string = trim(string, index + 1);
+	string = trim(string, expand(string, args, &outputLen));
+	if (string[0] is nullbyte)
+		return (outputLen);
+	return (outputLen + parse(string, args));
+}
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	char *string;
+	int outputLen;
+
+	string = makecopy(format);
+	if (string is NULL)
+		exit(98);
+	va_start(args, format);
+	outputLen = parse(string, args);
+	va_end(args);
+	free(string);
+	return (outputLen);
+}
